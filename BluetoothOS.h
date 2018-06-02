bd90a4eeba76329f62e29c7f1c247113c7141bd0
Christian Colucci <37155945+ktiOSz@users.noreply.github.com>
2018-06-02 17:20:46 +0200

    Create BluetoothOS.h

---
+++ BluetoothOS.h
@@ -0,0 +1,35 @@
+// BluetoothOS
+// Created by Christian Colucci KernelTeamiOS
+// Copyright © all right are served
+
+
+
+P0RT1NG iOS 11.3 w PS4 FIRMWARE BluetoothOS created by KernelTeamiOS
+
+
+— - - - - - - - - - - - - - - - - - 
+Device: iPhone 7 
+Software: iOS 11.3
+Jailbroken: NO
+root: YES
+sandbox: YES
+exploit: YES
+Bluetooth: 48:4B:AA:5A:9F:F7
+
+- - - - - - - - - - - - - - - - - - 
+Device: PS4 SLIM 2018
+Software: LAST UPDATE 06/18
+root: no
+exploit: beta
+Bluetooth: ktios.bluetoothOS
+
+— - - - - - - - - - - - - - - - - - 
+Porting: ktios.bluetoothOS
+host: ***********
+Password: **********
+exploit iOS: done
+exploit PS4: done / beta
+BluetoothOS: DONE
+
+
+// BluetoothOS done!
